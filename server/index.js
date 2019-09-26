3a5f9115fd44de6c47c18dccd3eb425d2ba667a3
apluxury <1091758704@qq.com>
2019-09-26 09:09:52 +0800

    增加模板引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const Koa = require('koa')
 const app = new Koa()
-const { normal } = require('./tpl')
+// const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
+const views = require('koa-views')
 const hostname = '127.0.0.1';
+const { resolve } = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
 app.use(async (ctx, next) => {
-    ctx.type = 'text/html;charset=utf-8'
-    ctx.body = normal
+    await ctx.render('index', {
+        you: 'Luke',
+        me: 'Scott'
+    })
 })
 
 app.listen(4455, hostname, () => {
---
+++ server/tpl/ejs.js
@@ -0,0 +1,29 @@
+
+module.exports = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koa Server HTML</title>
+    <link rel="stylesheet" href="https://cdn.bootcss.com/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+    <script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h1>Hi <%= you %></h1>
+                <p>This is <%= me %></p>
+            </div>
+            <div class="col-md-4">
+                <p>测试动态 EJS模板引擎</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+`
